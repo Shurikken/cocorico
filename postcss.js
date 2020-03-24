55368a66dcad2279d77582bef5762b5b32f89931
Valentin Chevoleau <valentin.chevoleau@perceptiondigitale.fr>
2020-03-24 16:08:51 +0100

    change patials

--- postcss.js
+++ postcss.js
@@ -1,23 +1,18 @@
-
 class TailwindExtractor {
-	static extract(content) {
-		return content.match(/[A-z0-9-:\/
-        ]+/g)
+    static extract(content) {
+        return content.match(/[A-z0-9-:\/]+/g)
     }
 }
 
-module.exports = {    
-  plugins: [        
-    require('postcss-import')({
-      path: [
-                "assets/css"
-            ],
-        }), 
-    require('tailwindcss')('./assets/css/tailwind.js'),    
-    require('autoprefixer')({
-      grid: true,
-      browsers: ['>1%'
-            ]
+module.exports = {
+    plugins: [
+        require('postcss-import')({
+            path: ["assets/css"],
+        }),
+        require('tailwindcss')('./assets/css/tailwind.js'),
+        require('autoprefixer')({
+            grid: true,
+            browsers: ['>1%']
         }),
     ]
 }
